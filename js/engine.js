3f7f53ce0eb678fcc26a19b4fb4453e01cf644f4
elecay <elecay@gmail.com>
2013-05-20 16:53:01 +0200

    Sort fixed. Version beta-0.1 done

--- js/engine.js
+++ js/engine.js
@@ -24,154 +24,183 @@
 
 (function () {
 
-    SDCARD = "sdcard";
-    filesToImport = [];
-    folders = [];
-    root = "";
-    isBacking = false;
-    foldersAdded = [];
+  SDCARD = "sdcard";
+  filesToImport = [];
+  folders = [];
+  root = "";
+  isBacking = false;
+  foldersAdded = [];
 
-    storage = navigator.getDeviceStorage(SDCARD);
+  storage = navigator.getDeviceStorage(SDCARD);
 
-    refreshBtn = document.querySelector("#refreshBtn");
-    refreshBtn.addEventListener ('click', function () {
-      load();
-    });
-
-    backhBtn = document.querySelector("#backBtn");
-    backhBtn.addEventListener ('click', function () {
-      back();
-    });
-
+  refreshBtn = document.querySelector("#refreshBtn");
+  refreshBtn.addEventListener ('click', function () {
     load();
+  });
 
-    function back(){
-      folders = root.split("/");
-      folders.splice(folders.length - 1, 1)
-      root = folders.join("/");
-      load();
+  backhBtn = document.querySelector("#backBtn");
+  backhBtn.addEventListener ('click', function () {
+    back();
+  });
+
+  load();
+
+  function back(){
+    isBacking = true;
+    folders = root.split("/");
+    folders.splice(folders.length - 1, 1)
+    root = folders.join("/");
+    load();
+  }
+
+  function load(){
+
+    foldersToSort = [];
+    filesToSort = [];
+    pathsToSort = [];
+    sizes = [];
+
+    alreadyAdded = [];
+    if(root == ""){
+      backhBtn.style.display = 'none';
+    } else {
+      backhBtn.style.display = 'block';
     }
 
-    function load(){
+    root_ = document.querySelector("#root_path");
+    root_.innerHTML = root;
 
-      alreadyAdded = [];
-      if(root == ""){
-        backhBtn.style.display = 'none';
-      } else {
-        backhBtn.style.display = 'block';
+    $('#item-list li').remove();
+    var cursor = storage.enumerate(root); 
+
+    cursor.onsuccess = function() {
+      if (!cursor.result)  {
+        execute();
+        return; 
       }
 
-      root_ = document.querySelector("#root_path");
-      root_.innerHTML = root;
+       var file = cursor.result;
+      if(file.name.split("/").length > 1){
+        pathsToSort.push(file.name);
+      } else {
+        filesToSort.push(file.name + " - " + (file.size / 1000000).toFixed(2) + "Mb");
+      }
+      cursor.continue(); 
+    }
 
-      $('#item-list li').remove();
-      var all_files = storage.enumerate(root); 
-      
-      all_files.onsuccess = function() {
-       
-        foldersToSort = [];
-        filesToSort = [];
-        sizes = [];
-        while (all_files.result)  {
-           var each_file = all_files.result;
-          n = each_file.name.split("/");
-          if(n.length == 1) {
-            filesToSort.push(n[0] + " - " + (each_file.size/1000000).toFixed(2) + "MB");
+    function execute() {
+
+      pathsToSort.sort(
+        function(a, b) {
+          if (a.toLowerCase() < b.toLowerCase()) return -1;
+          if (a.toLowerCase() > b.toLowerCase()) return 1;
+          return 0;
+        }
+      );
+      filesToSort.sort(
+        function(a, b) {
+          if (a.toLowerCase() < b.toLowerCase()) return -1;
+          if (a.toLowerCase() > b.toLowerCase()) return 1;
+          return 0;
+        }
+      );
+
+      for (var s = 0; s < pathsToSort.length; s++)  {
+        n = pathsToSort[s].split("/");
+        if(n.length == 1) {
+          filesToSort.push(n[0]);
+        } else {
+          foldersToSort.push(n[0]);
+        }
+      }
+
+      for (var g = 0; g < foldersToSort.length; g++)  {
+        path = foldersToSort[g].split("/");
+        if(alreadyAdded.lastIndexOf(path[0] + "/") == -1) {
+          alreadyAdded.push(path[0] + "/");
+          $("#item-list").append('<li><label><input type="checkbox"><span class="folder"></span>'
+            + '</label>' + path[0] + "/" + '</li>');
+        }
+      }
+      for (var f = 0; f < filesToSort.length; f++)  {
+        path = filesToSort[f].split("/");
+        $("#item-list").append('<li><label><input type="checkbox"><span class="file"></span>'
+          + '</label>' + path + '</li>');
+      }
+
+      flagOk = true;
+
+      $('#item-list').click(function(event) {
+        var target = $(event.target);
+        if(flagOk){
+          if(target.text().split("/").length > 1){
+            if(!isBacking){
+              if(root == ""){
+                root = target.text().substring(0, target.text().lastIndexOf('/'));
+              } else {
+                root = root + "/" + target.text().substring(0, target.text().lastIndexOf('/'));
+              }
+            }
+            load();
           } else {
-            foldersToSort.push(n[0]);
+            console.log("File to share: " + target.text());
+            importFiles(target.text().substring(0, target.text().lastIndexOf(' -')));
           }
-          all_files.continue(); 
+          flagOk = false;
         }
+      });
+    };
+    isBacking = false;
+  }
 
-        filesToSort.sort();
-        foldersToSort.sort();
+  function importFiles(filesToImport) {
 
-        for (var g = 0; g < foldersToSort.length; g++)  {
-          path = foldersToSort[g].split("/");
-          if(alreadyAdded.lastIndexOf(path[0] + "/") == -1) {
-            alreadyAdded.push(path[0] + "/");
-            $("#item-list").prepend('<li><label><input type="checkbox"><span class="folder"></span>'
-              + '</label>' + path[0] + "/" + '</li>');
-          }
-        }
-        for (var f = 0; f < filesToSort.length; f++)  {
-          path = filesToSort[f].split("/");
-          $("#item-list").append('<li><label><input type="checkbox"><span class="file"></span>'
-            + '</label>' + path + '</li>');
-        }
-        flagError = true;
-        flagOk = true;
+    a_file = (root == "") ? storage.get(filesToImport) : a_file = storage.get(root + "/" + filesToImport); 
 
-        $('#item-list').click(function(event) {
-          var target = $(event.target);
-           if(flagOk){
-            if(target.text().split("/").length > 1){
-              if(!isBacking){
-                if(root == ""){
-                  root = target.text().substring(0, target.text().lastIndexOf('/'));
-                }else{
-                  root = root + "/" + target.text().substring(0, target.text().lastIndexOf('/'));
-                }
-              }
-              load();
-            } else {
-              console.log("File to share: " + target.text());
-              importFiles(target.text().split(" -")[0]);
-            }
-            flagOk = false;
-          }
-        });
+    a_file.onerror = function() {
+      var afterNotification = function(){
+        Lungo.Router.section("main");
+        load();
       };
-      isBacking = false;
-    }
+      Lungo.Notification.error(
+        "Sorry!",
+        "We can't find a file in your SDCARD (or you have to unplug your phone).",
+        "warning",
+        5,
+        afterNotification
+        );
+      console.error("Error in: ", a_file.error.name);
+    };
 
-    function importFiles(filesToImport) {
+    a_file.onsuccess = function() { 
+      blob = a_file.result;
+      item = new Object();
+      item.isVideo = true,
+      item.filename = blob.name,
+      item.blob = blob
+      //to share with the email we have to do this (ugly)
+      var type = 'image/*';
+      var nameonly = item.filename.substring(item.filename.lastIndexOf('/') + 1);
+      var activity = new MozActivity({
+        name: 'share',
+        data: {
+          type: type,
+          number: 1,
+          blobs: [item.blob],
+          filenames: [nameonly],
+          filepaths: [item.filename]
+        }
+      });
 
-          a_file = (root == "") ? storage.get(filesToImport) : a_file = storage.get(root + "/" + filesToImport); 
+      activity.onerror = function(e) {
+        console.warn('Share activity error:', activity.error.name);
+        load();
+      };
 
-          a_file.onerror = function() {
-            var afterNotification = function(){
-              Lungo.Router.section("main");
-            };
-            Lungo.Notification.error(
-              "Sorry!",
-              "We can't find a file in your SDCARD (or you have to unplug your phone).",
-              "warning",
-              5,
-              afterNotification
-            );
-            console.error("Error in: ", a_file.error.name);
-          };
-
-          a_file.onsuccess = function() { 
-            blob = a_file.result;
-            item = new Object();
-            item.isVideo = true,
-            item.filename = blob.name,
-            item.blob = blob
-            //to share with the email we have to do this (ugly)
-            var type = 'image/*';
-            var nameonly = item.filename.substring(item.filename.lastIndexOf('/') + 1);
-            var activity = new MozActivity({
-              name: 'share',
-              data: {
-                type: type,
-                number: 1,
-                blobs: [item.blob],
-                filenames: [nameonly],
-                filepaths: [item.filename]
-              }
-            });
-
-            activity.onerror = function(e) {
-              console.warn('Share activity error:', activity.error.name);
-              load();
-            };
-
-            activity.onsuccess = function(e) {
-              load();
-            }
-          };
-    }
+      activity.onsuccess = function(e) {
+        load();
+      }
+    };
+  }
 
 })();
